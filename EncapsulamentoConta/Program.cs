23471f132a80e9e57524a74faa3b715c2c3fc6e1
Your Name <you@example.com>
2026-03-09 21:59:49 -0300

    encapsulamento

---
+++ EncapsulamentoConta/Program.cs
@@ -0,0 +1,9 @@
+﻿using EncapsulamentoConta;
+Conta c = new Conta();
+c.Nunero = 1; //setNumero(1);
+Console.WriteLine("Numero;" + c.Numero); //getNumero()
+//if (c.Numero > 5)
+//x = c.Numero + 21;
+
+c.Titular = "Jorge"
+c.saldo = 100:
